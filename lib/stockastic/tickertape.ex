bb5437f0c60af7a0c9637b6ebc44fc02ead70689
James Sanders <sanderjd@gmail.com>
2015-12-17 21:50:58 -0700

    Initial tickertape code

---
+++ lib/stockastic/tickertape.ex
@@ -0,0 +1,24 @@
+defmodule Stockastic.Tickertape do
+  def connect!(account, exchange) do
+    ws = Socket.Web.connect!("www.stockfighter.io", path: "/ob/api/ws/#{account}/venues/#{exchange}/tickertape", secure: true)
+    [ account, exchange, ws ]
+  end
+
+  def loop!([ account, exchange, ws ], callback) do
+    case Socket.Web.recv(ws) do
+      {:ok, {:text, text}} ->
+        callback.(text)
+      {:ok, {:ping, cookie}} ->
+        IO.puts "Got ping"
+        Socket.Web.pong(ws, cookie || "")
+      {:ok, {:close, :protocol_error, reason }} ->
+        IO.puts "Received close (#{reason}), reconnecting…"
+        [ _, _, ws ] = connect!(account, exchange)
+      packet ->
+        IO.puts "Unknown packet: #{inspect(packet)}"
+        [ _, _, ws ] = connect!(account, exchange)
+    end
+    loop!([ account, exchange, ws ], callback)
+  end
+  
+end
